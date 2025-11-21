8a490dfd261fdb427351ee4ec08dfaadc20c3935
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2025-11-21 16:14:34 -0300

    Remove: Removing duplicate files interface

--- src/use_case/duepay_use_case_interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from src.main.http_types.http_request.http_request import HttpRequest
-from src.main.http_types.http_response.http_response import HttpResponse
-
-class DuepayUseCaseInterface(ABC):
-  
-  @abstractmethod
-  def generate_report(self, http_request: HttpRequest) -> HttpResponse:
-    pass
